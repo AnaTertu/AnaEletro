40e72aaa6a579a04bda8b69f8d1d54b829096ed2
Ana Tertuliano <ana.tertu.2019@gmail.com>
2020-12-28 05:39:26 -0300

    Opção por não exibir senha, e estou exibindo a data.

--- frontend/components/Register/Card.jsx
+++ frontend/components/Register/Card.jsx
@@ -1,7 +1,7 @@
 const Card = () => {
     const [people, setPeople] = React.useState([])
     const [render, setRender] = React.useState(false)
-    const [msg, setMsg] = React.useState(false)
+    const [msn, setMsn] = React.useState(false)
 
     React.useEffect(async () => {
         const url = "http://www.localhost/react-php/backend";
@@ -15,7 +15,7 @@ const Card = () => {
 
         let formData = new FormData(event.target);
 
-        const url = "http://www.localhost/react-php/backend/register-client.php";
+        const url = "http://www.localhost/react-php/backend/register-client";
 
         fetch(url, {
             method: "POST",
@@ -24,10 +24,10 @@ const Card = () => {
             .then((response) => response.json())
             .then((dados)=> {
                 setRender(!render);
-                setMsg(dados);
+                setmsn(dados);
 
                 setTimeout(() => {
-                    setMsg(false);
+                    setmsn(false);
                 }, 3000);
         })
     }
@@ -35,18 +35,27 @@ const Card = () => {
     return (
         <div clasName="container py-5">
 
-            {people.map((element) => {
+            {people.map((person) => {
                 return (
-                    <div key={element.id} className="card w-50 mx-auto mt-3"> 
+                    <div key={person.idpeople} className="card w-50 mx-auto mt-3 "> 
                         <div className="card-header">
-                            {element.names}
+                            Nome: {person.nameC}
+                        </div>
+                        <div className="card-header" >
+                            E-mail: {person.email}
                         </div>
                         <div className="card-body">
-                            Telefone: {element.phones}
+                            Telefone: {person.phones}
                         </div>
                         <div className="card-footer">
-                            mensagem: {element.posts}
+                            Mensagem: {person.posts}
                         </div>
+                        <div className="card-footer">
+                            Data: {person.dates}
+                        </div>
+                        {/* <div className="card-footer">
+                            Senha: {person.passwords}
+                        </div> */}
                     </div>
                 )
             })}
