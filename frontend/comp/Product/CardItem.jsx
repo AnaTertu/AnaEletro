4916bc439ef865534405de64c3aae8afbc60b3de
Ana Tertuliano <ana.tertu.2019@gmail.com>
2021-01-01 11:54:43 -0300

    Zom ok em todos os produtos

--- frontend/comp/Product/CardItem.jsx
+++ frontend/comp/Product/CardItem.jsx
@@ -1,7 +1,7 @@
 const CardItem = (props) => {
 
-    function zoom() {
-        const cardItem = document.getElementById('cardItem');
+    function zoom(event) {
+        const cardItem = event.currentTarget.querySelector('img');
         if (cardItem.style.width == "100px") {
             cardItem.style.width = "200px"
         } else {
@@ -13,11 +13,13 @@ const CardItem = (props) => {
         <div
             className="card mx-auto mt-5 d-inline-block justify-content-sm-center"
             style={{ width: "300px" }}
-            onClick={zoom}>
+            onClick={zoom}
+        >
 
             <div className="card-bady d-flex justify-content-center">
                 <img width="100" id="cardItem" src={props.image} alt={props.category}></img>
             </div>
+            
             <div className="card-header text-center">
                 <h5>
                     {props.description}
