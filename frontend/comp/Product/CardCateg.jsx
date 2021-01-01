fcd1e75312e27672bdf270e79339840ec4db1018
Ana Tertuliano <ana.tertu.2019@gmail.com>
2021-01-01 15:55:57 -0300

    Concluido category

--- frontend/comp/Product/CardCateg.jsx
+++ frontend/comp/Product/CardCateg.jsx
@@ -8,29 +8,49 @@ const CardCateg = (props) => {
     }, []);
 
     return (
+
         <div className="card w-40 h-50 mx-1 mt-3">
             <h4> Categoria </h4>
             <ul>
                 <li>
                     Todos ({props.products.length})
-            </li>
-                <li>
-                    Geladeiras ({props.products.filter(product => product.idCategory == 1).length})
-            </li>
-                <li>
-                    Fogões ({props.products.filter(product => product.idCategory == 2).length})
-            </li>
-                <li>
-                    Microondas ({props.products.filter(product => product.idCategory == 3).length})
-            </li>
-                <li>
-                    Lavadoura de roupa ({props.products.filter(product => product.idCategory == 4).length})
-            </li>
-                <li>
-                    Lava-louças ({props.products.filter(product => product.idCategory == 5).length})
-            </li>
+                </li>
+                {categories.map((categ) =>(
+                    <React.Fragment key={categ.idCategory}>
+                        <li>
+                            {categ.category} ({props.products.filter(product => product.idCategory == categ.idCategory).length})
+                        </li>
+                    </React.Fragment>
+                )
+                )}
+               
+               
             </ul>
         </div>
+
+        // <div className="card w-40 h-50 mx-1 mt-3">
+        //     <h4> Categoria </h4>
+        //     <ul>
+        //         <li>
+        //             Todos ({props.products.length})
+        //     </li>
+        //         <li>
+        //             Geladeiras ({props.products.filter(product => product.idCategory == 1).length})
+        //          </li>
+        //         <li>
+        //             Fogões ({props.products.filter(product => product.idCategory == 2).length})
+        //     </li>
+        //         <li>
+        //             Microondas ({props.products.filter(product => product.idCategory == 3).length})
+        //     </li>
+        //         <li>
+        //             Lavadoura de roupa ({props.products.filter(product => product.idCategory == 4).length})
+        //     </li>
+        //         <li>
+        //             Lava-louças ({props.products.filter(product => product.idCategory == 5).length})
+        //     </li>
+        //     </ul>
+        // </div>
 
     );
 
