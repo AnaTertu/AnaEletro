5842cec3fb03c01760f87aa9b98ffe6482873467
Ana Tertuliano <ana.tertu.2019@gmail.com>
2021-01-03 20:42:17 -0300

    fix connection

--- backend/Models/Connection.php
+++ backend/Models/Connection.php
@@ -4,14 +4,22 @@ class Connection
 
     public static function getDb()
     {
-        $servername = "192.168.0.164";
-        $username = "anatertu";
-        $password = "123456";
-        $database = "fseletro";
+        // $servername = "192.168.0.164";
+        // $username = "anatertu";
+        // $password = "123456";
+        // $database = "fseletro";
+
+        //Criar as constantes com as credencias de acesso ao banco de dados
+        define('HOST', '192.168.0.164');
+        define('USER', 'anatertu');
+        define('PASS', '123456');
+        define('DBNAME', 'fseletro');
+        define('PORT', '3306');
 
         try {
             echo "!!!Iniciando conexão!!!";
-            $conn = new PDO("mysql:host=", $servername, ";dbname=", $database, $username, $password);
+            //Criar a conexão com banco de dados usando o PDO
+            $conn = new pdo('mysql:host=' . HOST . ';port=' . PORT . ';dbname=' . DBNAME, USER, PASS);
             echo "<h1>sucesso conexão</h1>";
         } catch (PDOException $e) {
             echo "Erro: Conexão com banco de dados não foi realizada com sucesso. Erro gerado " . $e->getMessage();
